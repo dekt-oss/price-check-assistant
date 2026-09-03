e894186d91298fae33e5988d9d2b57628eee1663
dekt-oss <degackt@gmail.com>
2026-09-04 07:37:42 +0900

    feat: add hardened manufacturer public-price source (#17)

--- src/purchase_price/collectors/registry.py
+++ src/purchase_price/collectors/registry.py
@@ -1,9 +1,16 @@
 from .base import PriceCollector
+from .manufacturer_public_catalog import ManufacturerPublicCatalogCollector
 from .mock_public import MockPublicCollector
 
 
-def build_collectors(include_mock: bool = True) -> list[PriceCollector]:
+def build_collectors(
+    *,
+    include_mock: bool = True,
+    include_manufacturer_public: bool = True,
+) -> list[PriceCollector]:
     collectors: list[PriceCollector] = []
+    if include_manufacturer_public:
+        collectors.append(ManufacturerPublicCatalogCollector())
     if include_mock:
         collectors.append(MockPublicCollector())
     return collectors
