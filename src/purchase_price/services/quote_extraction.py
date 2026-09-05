afb53480acca7c6f87e54a9c0ec9d23c1fff9ec3
dekt-oss <degackt@gmail.com>
2026-09-06 01:02:44 +0900

    Fix quote guard module lint

--- src/purchase_price/services/quote_extraction.py
+++ src/purchase_price/services/quote_extraction.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import re
 from dataclasses import replace
 
-from purchase_price.services import quote_extraction_core as _core
 from purchase_price.services.quote_extraction_core import *  # noqa: F403
+from purchase_price.services import quote_extraction_core as _core
 
 
 _SUMMARY_LABELS = frozenset(
